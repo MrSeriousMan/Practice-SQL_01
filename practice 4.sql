4a1261bbfa43aebc922b30da2127ad7cf5d44ba9
MrSeriousMan <99726760+MrSeriousMan@users.noreply.github.com>
2023-11-16 16:10:54 +0700

    practice 4.sql

--- practice 4.sql
+++ practice 4.sql
@@ -5,7 +5,10 @@ SELECT
 FROM viewership;
 
 /*Ex2:Triangel judgement*/
-SELECT *, IF(x+y>z and y+z>x and z+x>y, "Yes", "No") as triangle FROM Triangle
+select 
+x,y,z,
+case when (x+y) > z and (x+z) > y and (y+z) > x then 'Yes' else 'No' end as triangle
+from Triangle 
 
 /*Ex3:UnitedHealth Group*/
 SELECT 
